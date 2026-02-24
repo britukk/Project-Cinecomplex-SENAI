319d60423f7acda781ff3329c7fc848e1c9413ed
britukk <jpbsmeira@gmail.com>
2026-02-24 20:18:07 -0300

    Início do Projeto

---
+++ cinema/src/main/java/com/cinecomplex/app/models/Filmes.java
@@ -0,0 +1,15 @@
+package com.cinecomplex.app.models;
+
+import java.time.LocalDate;
+
+public class Filmes {
+
+    public int idFilmes;
+    public int duracao;
+    public LocalDate dataLancamento;
+    public String titulo;
+    public String sinopse;
+    public String idioma;
+
+
+}
